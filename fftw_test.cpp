bfd9e9eebf479af77d5a0fd5d4fef7d41a6953ec
wisatbff <wisatbff@gmail.com>
2014-06-15 02:01:11 +0800

    85% done

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -6,32 +6,52 @@ using namespace std;
 
 int main(void)
 {
-    double *in;
-    fftw_complex *out;
-    fftw_plan p, ip;
+    double *in, *in2, *in_m;
+    fftw_complex *out, *out2, *out_m;
+    fftw_plan p, p2, ip;
 
     in = (double*) fftw_malloc(sizeof(double) * N);
+    in2 = (double*) fftw_malloc(sizeof(double) * N);
+    in_m = (double*) fftw_malloc(sizeof(double) * N);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    out2 = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    out_m = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
 //    p = fftw_plan_r2r_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
     p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE);
-    ip = fftw_plan_dft_c2r_1d(N, out, in, FFTW_ESTIMATE);
+    p2 = fftw_plan_dft_r2c_1d(N, in2, out2, FFTW_ESTIMATE);
+//    ip = fftw_plan_dft_c2r_1d(N, out, in, FFTW_ESTIMATE);
+    ip = fftw_plan_dft_c2r_1d(N, out_m, in_m, FFTW_ESTIMATE);
 
     in[0] = 1.0;
     in[1] = 0;
-    in[2] = 0;
+    in[2] = 1.0;
     in[3] = 0;
 
-    fftw_execute(p); /* repeat as needed */
-    for (int i = 0; i < N; ++i)
-        cout << out[i][0] << " + j" << out[i][1] << endl;
+    in2[0] = 1.0;
+    in2[1] = 1.0;
+    in2[2] = 0;
+    in2[3] = 0;
 
+    fftw_execute(p); /* repeat as needed */
+    fftw_execute(p2);
+//    for (int i = 0; i < N; ++i)
+//        cout << out[i][0] << " + j" << out[i][1] << endl;
+
+    for (int i = 0; i < N/2 + 1; ++i) {
+        out_m[i][0] = (out[i][0]*out2[i][0] - out[i][1]*out2[i][1])/N;
+        out_m[i][1] = (out[i][0]*out2[i][1] + out[i][1]*out2[i][0])/N;
+    }
     fftw_execute(ip);
     for (int i = 0; i < N; ++i)
-        cout << in[i]/N << " ";
+        cout << in_m[i] << " ";
     cout << endl;
 
     fftw_destroy_plan(p);
     fftw_free(in);
+    fftw_free(in2);
+    fftw_free(in_m);
     fftw_free(out);
+    fftw_free(out2);
+    fftw_free(out_m);
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#pragma GCC diagnostic ignored "-fpermissive"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <fftw3.h>
@@ -6,6 +7,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include "main.h"
 
 #define  TS         0.001
 // 采样频率
@@ -14,9 +16,11 @@
 #define  F1         10000
 #define  F2         40000
 #define  PI         3.1415926535897932
+#define  AMP        5
 
 double freq_one[NTMINT], freq_zero[NTMINT];
-double deviation = 1;
+double deviation = 50;
+int pixel_cnt;
 
 using namespace std;
 using namespace cv;
@@ -25,6 +29,7 @@ using namespace cv;
 static void modulate(const uchar intensity, double *txData[]);
 static void signal_channel(double *txData[]);
 static uchar demodulate(const double *txData[]);
+static void filter(const double *in, const double *filt, double *outp);
 static double gaussrand(void);
 
 int main(void)
@@ -34,19 +39,19 @@ int main(void)
 //        cerr << "Could not open or find the image" << endl;
 //        return 1;
 //    }
-//    namedWindow( "Original image", WINDOW_AUTOSIZE);
-//    imshow( "Original image", image);
+//    namedWindow("Original image", WINDOW_AUTOSIZE);
+//    imshow("Original image", image);
 
-    Mat src(1, 1, CV_8UC1, Scalar(170));
+    Mat src(1, 1, CV_8UC1, Scalar(155));
 
     srand(time(NULL));
 
     ofstream ff1("f1.dat");
     ofstream ff2("f2.dat");
     for (int i = 0; i < NTMINT; ++i) {
-        freq_one[i] = 5*cos(2*PI*F1*i*TDELTA);
+        freq_one[i] = AMP*cos(2*PI*F1*i*TDELTA);
         ff1 << i*TS/NTMINT << " " << freq_one[i] << endl;
-        freq_zero[i] = 5*cos(2*PI*F2*i*TDELTA);
+        freq_zero[i] = AMP*cos(2*PI*F2*i*TDELTA);
         ff2 << i*TS/NTMINT << " " << freq_zero[i] << endl;
     }
     ff1.close();
@@ -71,19 +76,22 @@ int main(void)
                     ftx_pn << i*TS+j*TS/NTMINT << " " << txData[i][j] << endl;
             }
             ftx_pn.close();
-//
-//            uchar rxData;
-//            rxData = demodulate(txData);
+
+            uchar rxData = 0x00;
+            rxData = demodulate(txData);
 
             for (int i = 0; i < 8; ++i)
                 fftw_free(txData[i]);
+            ++pixel_cnt;
+            if (pixel_cnt == src.rows * src.cols) {
+                cout << (unsigned int)rxData << endl;
+                pixel_cnt = 0;
+            }
         }
     }
 
     return 0;
-}
-
-static void modulate(const uchar intensity, double *txData[])
+} static void modulate(const uchar intensity, double *txData[])
 {
     for (int i = 0; i < 8; ++i) {
         txData[i] = (double *)fftw_malloc(sizeof(double) * NTMINT * 8);
@@ -104,6 +112,59 @@ static void signal_channel(double *txData[])
 
 static uchar demodulate(const double *txData[])
 {
+    static double c1[8][NTMINT], c2[8][NTMINT];
+    static uchar pixel = 0x00;
+    for (int i = 0; i < 8; ++i) {
+        // 上路F1解调
+        // 带通滤波器
+        filter(txData[i], bpf1, c1[i]);
+        // 乘法器
+        for (int j = 0; j < NTMINT; ++j)
+            c1[i][j] *= freq_one[j];
+        // 低通滤波器
+        filter(c1[i], lpf, c1[i]);
+
+        // 下路F2解调
+        filter(txData[i], bpf2, c2[i]);
+        for (int j = 0; j < NTMINT; ++j)
+            c2[i][j] *= freq_zero[j];
+        filter(c2[i], lpf, c2[i]);
+
+        if (c1[i][500] > c2[i][500])
+            pixel |= 1 << i;
+    }
+
+    ofstream fch_outp1("ch_outp1.dat");
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < NTMINT; ++j)
+            fch_outp1 << i*TS+j*TS/NTMINT << " " << c1[i][j] << endl;
+    }
+    fch_outp1.close();
+    ofstream fch_outp2("ch_outp2.dat");
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < NTMINT; ++j)
+            fch_outp2 << i*TS+j*TS/NTMINT << " " << c2[i][j] << endl;
+    }
+    fch_outp2.close();
+
+    return pixel;
+}
+
+static void filter(const double *in, const double *filt, double *outp)
+{
+    static fftw_complex ffx[NTMINT], fff[NTMINT], ffm[NTMINT];
+    static fftw_plan px, pf, ip;
+
+    px = fftw_plan_dft_r2c_1d(NTMINT, in, ffx, FFTW_ESTIMATE);
+    pf = fftw_plan_dft_r2c_1d(NTMINT, filt, fff, FFTW_ESTIMATE);
+    fftw_execute(px);
+    fftw_execute(pf);
+    for (int j = 0; j < NTMINT; ++j) {
+        ffm[j][0] = (ffx[j][0]*fff[j][0] - ffx[j][1]*fff[j][1])/NTMINT;
+        ffm[j][1] = (ffx[j][0]*fff[j][1] + ffx[j][1]*fff[j][0])/NTMINT;
+    }
+    ip = fftw_plan_dft_c2r_1d(NTMINT, ffm, outp, FFTW_ESTIMATE);
+    fftw_execute(ip);
 }
 
 static double gaussrand(void)
